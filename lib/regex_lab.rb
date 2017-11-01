524051b2518e089ebe9529bc0678c8af4a4292ab
Maryna Voitenko <maryna.voitenko@gmail.com>
2017-11-01 02:29:18 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-   if word.scan(/\b[aeiouAEIOU]\w*/) return true
+  return true if word.scan(/\b[aeiouAEIOU]\w*/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
