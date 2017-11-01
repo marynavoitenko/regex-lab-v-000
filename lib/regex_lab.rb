0f54252a3e187c43e41f73adaf6ad177843099df
Maryna Voitenko <maryna.voitenko@gmail.com>
2017-11-01 03:12:11 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if text.match(/^[1-9]\d*/)
+  if text.match(/\(?\d+\)?[-.\s]?\d+[-.\s]?\d+/)
     return true
   else
     return false
